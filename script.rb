b565cdd5666df83f090c4fb2a23f678d44d569f1
MaeFire <masked_phoenix@yahoo.com>
2023-01-10 23:51:38 -0600

    Add visual feedback that shows more information
    I wanted to make a slightly real world response after putting numbers
    into the algorithm, so I printed out a response that shows the total
    profit, which indexes gave that value, and the values of those indexes.

--- script.rb
+++ script.rb
@@ -1,21 +1,29 @@
 require "pry-byebug"
 
 def stock_picker(prices)
-    lowest_price = prices[0] #starting point. lowest price is currently 17 as the base
-    low_p_index  = 0
-    current_sum  = 0
+    lowest_price            = prices[0] #starting point. lowest price is index[0] as the base
+    low_p_index             = ""
+    current_sum             = 0
+    index_with_greatest_sum = []
+    lowest_final            = 0 #variables for print-out
+    highest_final           = 0 #variables for print-out
 
     prices.each_with_index do |element, index|
         if element < lowest_price
             lowest_price = element
-            # p lowest_price # default numbers print = 3,1
-            low_p_index = index ## might not need?
-            # p low_p_index # default index print = 1,7 (matches to values)
+            low_p_index = index
             next
         end
+        if element - lowest_price > current_sum
+            current_sum = element - lowest_price
+            lowest_final = lowest_price
+            highest_final = element
+            index_with_greatest_sum = [low_p_index, index]
+        end
     end
-    
+    print ("profit of $#{current_sum} at positions #{index_with_greatest_sum},")
+    print (" where you must buy at $#{lowest_final} and sell at $#{highest_final}\n")
 end
 default_values = [17,3,6,9,15,8,6,1,10]
- stock_picker(default_values)
+stock_picker(default_values)
 #  [1,4]  # for a profit of $15 - $3 == $12
